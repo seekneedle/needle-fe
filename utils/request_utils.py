3633652bcdfc5684224ea4bde6678bb963529042
Huaxing.Wang <Huaxing.Wang@homecredit.cn>
2025-01-08 19:27:24 +0800

    Modify needle fe.

--- utils/request_utils.py
+++ utils/request_utils.py
@@ -1,67 +1,97 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from utils import files_utils
-import json
 from config import config
+from utils.security import decrypt
+import time
 
 username = config['username']
-password = config['password']
-base_url = config['ip'] + ":" + config['port']
+password = decrypt(config['password'])
+base_url = 'http://' + config['ip'] + ":" + str(config['port'])
 
 
-def create_kb(filepaths,index_name,max_length,overlap_length,segment_id):
-    url = f'{base_url}/vector_stores/create'
+def create_kb(uploaded_files,index_name,max_length,overlap_length,segment_id):
+    url = f'{base_url}/vector_store/create'
     files = []
-    for f in filepaths:
-        filename = f.split('/')[-1]
-        filetype = filename.split('.')[-1]
-        file_base64 = files_utils.file_to_base64(f)
-        language = 'cn'
-        file_info = {"file_name":filename,"file_base64":file_base64,"file_type":filetype,"language":language}
-        files.append(file_info)
+    for f in uploaded_files:
+        files.append(('files', (f.name, f.getbuffer())))
     
     data = {
-      "files":files,
-      "name":index_name,
-      "chunking_strategy":{"chunk_type":1,"max_tokens":max_length,"overlap":overlap_length,"separator":segment_id}
+        "name":index_name,
+        "chunk_size": max_length,
+        "overlap_size": overlap_length
     }
     
-    response = requests.post(url, auth=HTTPBasicAuth(username, password), data=json.dumps(data))
+    response = requests.post(url, auth=HTTPBasicAuth(username, password), data=data, files=files)
+
+    task_id = response.json()['data']['task_id']
+
+    status_url = f'{base_url}/vector_store/task_status/{task_id}'
+
+    counter = 0
+
+    while counter < 50:
+        status_response = requests.get(status_url, auth=HTTPBasicAuth(username, password))
+
+        status_data = status_response.json()['data']
+
+        if status_data['status'] == 'COMPLETED' or status_data['status'] == 'FAILED':
+            return status_response
+
+        time.sleep(6)
 
     return response
 
 
-def upload_files(filepaths,index_id,max_length,overlap_length,segment_id):
-    url = f"{base_url}/vector_stores/{index_id}/upload"
+def upload_files(uploaded_files,index_id,max_length,overlap_length,segment_id):
+    url = f'{base_url}/vector_store/file/add'
+
     files = []
-    for f in filepaths:
-        filename = f.split('/')[-1]
-        filetype = filename.split('.')[-1]
-        file_base64 = files_utils.file_to_base64(f)
-        language = 'cn'
-        file_info = {"file_name":filename,"file_base64":file_base64,"file_type":filetype,"language":language}
-        files.append(file_info)
-    
+    for f in uploaded_files:
+        files.append(('files', (f.name, f.getbuffer())))
+
     data = {
-      "files":files,
-      "chunking_strategy":{"chunk_type":1,"max_tokens":max_length,"overlap":overlap_length,"separator":segment_id}
+        "id": index_id
     }
-    
-    response = requests.post(url, auth=HTTPBasicAuth(username, password), data=json.dumps(data))
+
+    response = requests.post(url, auth=HTTPBasicAuth(username, password), data=data, files=files)
+
+    task_id = response.json()['data']['task_id']
+
+    status_url = f'{base_url}/vector_store/task_status/{task_id}'
+
+    counter = 0
+
+    while counter < 50:
+        status_response = requests.get(status_url, auth=HTTPBasicAuth(username, password))
+
+        status_data = status_response.json()['data']
+
+        if status_data['status'] == 'COMPLETED' or status_data['status'] == 'FAILED':
+            return status_response
+
+        time.sleep(6)
 
     return response
 
 
 def reload_files(reload_index_id):
-    url = f"{base_url}/vector_stores/{reload_index_id}/files"
+    url = f"{base_url}/vector_store/file/list/{reload_index_id}"
     response = requests.get(url, auth=HTTPBasicAuth(username, password))
     return response
 
 
-def delete_file(reload_index_id, file_name):
-    url = f"{base_url}/vector_stores/{reload_index_id}/files/{file_name}"
-    response = requests.delete(url, auth=HTTPBasicAuth(username, password))
+def delete_file(reload_index_id, file_id):
+    data = {
+        "id": reload_index_id,
+        "file_ids": [file_id]
+    }
+    url = f"{base_url}/vector_store/file/delete"
+    response = requests.post(url, auth=HTTPBasicAuth(username, password), json=data)
     return response
 
 
-
+def get_store_list():
+    url = f"{base_url}/vector_store/list"
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+    return response
--- webui.py
+++ webui.py
@@ -1,6 +1,7 @@
 import streamlit as st  
 import os 
 from utils import request_utils
+from config import config
 
   
 # 指定文件保存路径  
@@ -17,12 +18,22 @@ overlap_length = 30  # 假设默认重叠长度为30
 segment_id = "word"  # 默认分段标识符  
   
 # 应用布局  
-st.title("知识库创建更新")  
-  
+st.title("知识库管理页面")
+
+
+st.text('                                ')
+st.text('-------------------知识库列表------------------')
+st.text('                                 ')
+
+store_list = request_utils.get_store_list()
+st.write(store_list.json()['data']['vector_stores'])
+
+st.text('                                ')
+st.text('-------------------创建知识库------------------')
+st.text('                                 ')
+
 # 输入框  
-index_name = st.text_input("请输入知识库名称(创建):", value=index_name)  
-# 输入框  
-index_id = st.text_input("请输入知识库id(更新):", value=index_id)  
+index_name = st.text_input("请输入知识库名称(创建):", value=index_name)
   
 # 单选按钮  
 segmentation_modes = ["自定义", "自动分段与清洗"]  
@@ -35,31 +46,30 @@ if segmentation_mode == "自定义":
     segment_id = st.text_input("分段标识符:", value=segment_id)  
   
 # 文件上传  
-uploaded_files = st.file_uploader("请上传文件:", type=["txt", "docx", "pdf"],accept_multiple_files=True)
-  
-filepaths = []
-if uploaded_files:  
-    for i, file in enumerate(uploaded_files):
-        file_path = os.path.join('uploads', file.name)
-        with open(file_path, 'wb') as f:
-            f.write(file.getbuffer())
-        filepaths.append(file_path)
-        st.success(f'文件 {file.name} 已保存到 {file_path}')
-        
-  
+uploaded_files = st.file_uploader("请上传文件:", type=["txt", "docx", "pdf"],accept_multiple_files=True, key="uploaded_files")
+
 # 这里可以添加其他处理逻辑，如读取文件内容并显示，或根据用户输入处理文件等  
 # 创建数据库按钮
 if st.button('创建知识库'):
     #调用接口1
-    result = request_utils.create_kb(filepaths,index_name,max_length,overlap_length,segment_id)
+    result = request_utils.create_kb(uploaded_files,index_name,max_length,overlap_length,segment_id)
     #删除uploads下的文件
     #TODO
-    st.write(result.json()) 
-    
+    st.write(result.json())
+
+st.text('                                ')
+st.text('-------------------知识库上传文件------------------')
+st.text('                                 ')
+
+# 输入框
+index_id = st.text_input("请输入知识库id(更新):", value=index_id)
+
+# 文件上传
+uploaded_files1 = st.file_uploader("请上传文件:", type=["txt", "docx", "pdf"],accept_multiple_files=True, key="uploaded_files1")
 
 if st.button('上传/更新文件'):
     #调用接口2
-    result = request_utils.upload_files(filepaths,index_id,max_length,overlap_length,segment_id)
+    result = request_utils.upload_files(uploaded_files1,index_id,max_length,overlap_length,segment_id)
     st.write(result.json())
 st.text('                                ')
 st.text('-------------------查询文件------------------')
@@ -71,7 +81,7 @@ reload_index_id = st.text_input("请输入知识库id(查询所有文件):", val
 if st.button('查询'):
     #调用接口2
     result = request_utils.reload_files(reload_index_id)
-    st.write(result.json()["data"]["filenames"])
+    st.write(result.json()["data"]["documents"])
 
 st.text('                                ')
 st.text('-------------------删除文件------------------')
@@ -79,11 +89,11 @@ st.text('                                 ')
 delete_index_id = ""
 delete_index_id = st.text_input("请输入知识库id(删除文件):", value=delete_index_id)
 
-delete_file_name = ""
-delete_file_name = st.text_input("请输入文件名(删除文件):", value=delete_file_name)
+delete_file_id = ""
+delete_file_id = st.text_input("请输入文件doc_id(删除文件):", value=delete_file_id)
 
 if st.button('删除'):
-    result = request_utils.delete_file(delete_index_id, delete_file_name)
+    result = request_utils.delete_file(delete_index_id, delete_file_id)
     st.write(result.json())
 # 显示用户输入，作为示例  
 #st.write("输入参数：")  
